da58b0b80ba6b8b5c3cae9c14430163e0e64f192
Stefan Hilbert <stefanhilbert.12@gmail.com>
2023-01-19 00:13:50 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://localhost5000/', {
+    const response = await fetch('https://memphis.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
